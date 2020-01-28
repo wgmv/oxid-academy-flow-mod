9a3986feb447152a963371f8e06102e246f81245
Walter <walter@wgmv.de>
2020-01-28 17:51:54 +0100

    init

---
+++ theme.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Theme Information
+ */
+$aTheme = array(
+    'id'          => 'flow',
+    'title'       => 'Flow',
+    'description' => 'Flow mod created with OxidAcademy',
+    'thumbnail'   => 'theme.jpg',
+    'version'     => '1.0.0',
+    'author'      => 'wgmv',
+    'parentTheme'    => 'flow',
+    'parentVersions' => ['3.3'],
+);
